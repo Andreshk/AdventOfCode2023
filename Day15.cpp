4db497536bd5818ec80bfbbd7172ef9356606e00
Andrey Drenski <andreshko.dre@gmail.com>
2023-12-15 11:10:16 +0200

    add day15 solutions

---
+++ Day15.cpp
@@ -0,0 +1,62 @@
+#include "Lines.h"
+#include <scn/scan.h>
+#include <print>
+#include <vector>
+#include <ranges> // str::views::{transform,split,enumerate}
+#include <algorithm> // std::ranges::{fold_left,find_first_of}
+#include <cassert>
+
+uint8_t hash(std::string_view str) {
+	return std::ranges::fold_left(str, 0, [](uint8_t res, char c) { return uint8_t((res + c) * 17); });
+}
+
+int day15(const char* filename) {
+	std::string line;
+	std::ifstream{filename} >> line;
+	return ranges::sum(std::views::split(line, ',')
+		| std::views::transform([](auto&& chunk) { return std::string_view{chunk}; })
+		| std::views::transform(hash));
+}
+
+int day15_2(const char* filename) {
+	std::string line;
+	std::ifstream{filename} >> line;
+	struct Item {
+		std::string_view str;
+		int val;
+	};
+	std::array<std::vector<Item>, 256> map;
+	for (auto&& chunk : std::views::split(line, ',')) {
+		const auto mid = std::ranges::find_first_of(chunk, "-=");
+		assert(mid != chunk.end());
+		const std::string_view label{chunk.begin(), mid};
+		std::vector<Item>& bucket = map[hash(label)];
+		// Helper to find an item with the given label
+		auto f = [&](const Item& it) { return it.str == label; };
+		if (*mid == '-') {
+			std::erase_if(bucket, f);
+		} else {
+			const int val = scn::scan<int>(std::ranges::subrange(mid + 1, chunk.end()), "{}")->value();
+			if (const auto it = std::ranges::find_if(bucket, f); it == bucket.end()) {
+				bucket.emplace_back(label, val);
+			} else {
+				it->val = val;
+			}
+		}
+	}
+	int sum = 0;
+	for (auto&& [i, bucket] : std::views::enumerate(map)) {
+		for (auto&& [j, item] : std::views::enumerate(bucket)) {
+			sum += int((i + 1) * (j + 1) * item.val);
+		}
+	}
+	return sum;
+}
+
+int main15() {
+	std::println("{}", day15("input/15test.txt")); // 1320
+	std::println("{}", day15("input/15full.txt")); // 509152
+	std::println("{}", day15_2("input/15test.txt")); // 145
+	std::println("{}", day15_2("input/15full.txt")); // 244403
+	return 0;
+}
