bc46492a361f6bdae559b7736c8b82334ee00bf5
Andrey Drenski <andreshko.dre@gmail.com>
2023-12-18 13:36:54 +0200

    O(1) memory is actually enough

--- Day18.cpp
+++ Day18.cpp
@@ -2,16 +2,12 @@
 #include "IntPairs.h"
 #include <scn/scan.h>
 #include <print>
-#include <vector>
-#include <ranges> // std::views::pairwise_transform
-#include <algorithm> // std::ranges::find
 
 int64_t day18(const char* filename, const bool part1) {
-	const IntPair offs[4] = {{0,1},{1,0},{0,-1},{-1,0}};
-	std::vector<IntPair> coords;
+	const IntPair offs[4] = {{0,1},{1,0},{0,-1},{-1,0}}; // R D L U
 	IntPair curr{0,0};
-	coords.push_back(curr);
 	int64_t border = 0;
+	int64_t inside = 0;
 	for (std::string_view line : lines(filename)) {
 		const auto pos = line.find('#');
 		const int k = (part1
@@ -21,14 +17,11 @@ int64_t day18(const char* filename, const bool part1) {
 			? int(std::string_view{"RDLU"}.find(line[0]))
 			: line[pos + 6] - '0');
 		assert(dir >= 0 && dir < 4);
-		curr += (offs[dir] * k);
-		coords.push_back(curr);
+		const IntPair next = curr + (offs[dir] * k);
 		border += k;
+		inside += int64_t(curr.i) * next.j - int64_t(next.i) * curr.j; // Shoelace formula
+		curr = next;
 	}
-	// Instructions should give out a closed loop, required for flood filling and the Shoelace formula
-	assert(coords.back() == coords.front());
-	const int64_t inside = ranges::sum(std::views::pairwise_transform(coords,
-		[](IntPair p1, IntPair p2) { return int64_t(p1.i) * p2.j - int64_t(p2.i) * p1.j; }));
 	// The whole border is overcounting (we need +1, not +2)
 	return (border/2 + std::abs(inside / 2) + 1);
 }
